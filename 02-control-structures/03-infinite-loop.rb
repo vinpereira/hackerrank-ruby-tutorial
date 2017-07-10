f65df29238f27b46d70dbfac5708131cbe6cb297
Vinícius Pereira <jedipereira@gmail.com>
2017-07-10 14:53:31 -0300

    Solutions for Control Structures

---
+++ 02-control-structures/03-infinite-loop.rb
@@ -0,0 +1,6 @@
+# loop do-end is an infinite loop
+loop do
+    coder.practice
+    # exit from infinite loop if 'oh_one?' is true
+    break if coder.oh_one?
+end
---
+++ 02-control-structures/04-until.rb
@@ -0,0 +1,2 @@
+# repeat 'coder.practice' until 'oh_one?' returns true
+coder.practice until coder.oh_one?
